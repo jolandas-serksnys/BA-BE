630bc1a04494146c21d56dbe29ffeea4635ae5f8
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2022-04-06 21:08:52 +0300

    conf

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 import app from "./app";
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
