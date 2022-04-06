e2526a393c44c150d85bb31574f5009003667af4
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2022-04-06 20:52:29 +0300

    Updated

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 export const database = new Sequelize({
-  host: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'bp',
-  dialect: "mysql",
+  host: 'wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  username: 'phxxq5q9vej4qzrf',
+  password: 't2karusqrfi837g0',
+  database: 'lnpyotlag2fstj3h',
+  dialect: 'mysql',
   pool: {
     max: 5,
     min: 0,
@@ -14,3 +14,20 @@ export const database = new Sequelize({
   },
   logging: false
 });
+
+/*
+export const database = new Sequelize({
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'bp',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: false
+});
+*/
--- models/sync.ts
+++ models/sync.ts
@@ -11,23 +11,23 @@ import { Tag } from "./menu/tag";
 
 export const Sync = async () => {
   try {
-    // await Establishment.sync({force: true}).then(() => console.log("Establishments table created"));
-    // await Employee.sync({force: true}).then(() => console.log("Employees table created"));
+    await Establishment.sync({ force: true }).then(() => console.log("Establishments table created"));
+    await Employee.sync({ force: true }).then(() => console.log("Employees table created"));
 
-    // await Table.sync({force: true}).then(() => console.log("Tables table created"));
-    // await TableClaim.sync({ force: true }).then(() => console.log("Table claims table created"));
-    // await Customer.sync({ force: true }).then(() => console.log("Customers table created"));
+    await Table.sync({ force: true }).then(() => console.log("Tables table created"));
+    await TableClaim.sync({ force: true }).then(() => console.log("Table claims table created"));
+    await Customer.sync({ force: true }).then(() => console.log("Customers table created"));
 
-    // await Category.sync({ force: true }).then(() => console.log("Categories table created"));
-    // await Dish.sync({ force: true }).then(() => console.log("Dishes table created"));
-    // await Tag.sync({ force: true }).then(() => console.log("Tags table created"));
+    await Category.sync({ force: true }).then(() => console.log("Categories table created"));
+    await Dish.sync({ force: true }).then(() => console.log("Dishes table created"));
+    await Tag.sync({ force: true }).then(() => console.log("Tags table created"));
 
-    // await Addon.sync({ force: true }).then(() => console.log("Addons table created"));
-    // await Option.sync({ force: true }).then(() => console.log("Addons table created"));
+    await Addon.sync({ force: true }).then(() => console.log("Addons table created"));
+    await Option.sync({ force: true }).then(() => console.log("Addons table created"));
 
-    // await TableOrder.sync({ force: true }).then(() => console.log("Table Orders table created"));
-    // await CustomerOrder.sync({ force: true }).then(() => console.log("Customer Orders table created"));
-    // await OrderAddon.sync({ force: true }).then(() => console.log("Orders addons table created"));
+    await TableOrder.sync({ force: true }).then(() => console.log("Table Orders table created"));
+    await CustomerOrder.sync({ force: true }).then(() => console.log("Customer Orders table created"));
+    await OrderAddon.sync({ force: true }).then(() => console.log("Orders addons table created"));
   } catch (error) {
     console.error(error);
   }
