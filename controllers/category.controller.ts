f5140279733f49b76a51e2785bc5d6371bd2670d
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2022-05-06 08:21:04 +0300

    Code cleanup

--- controllers/category.controller.ts
+++ controllers/category.controller.ts
@@ -1,11 +1,9 @@
 import {
-  Category,
-  CategoryInterface,
-  Employee
+  Category
 } from "../models";
 import { ResponseType } from "../utils";
 import { Request, Response } from "express";
-import { DestroyOptions, UpdateOptions } from "sequelize";
+import { DestroyOptions } from "sequelize";
 
 const MESSAGE_CREATE = 'Category was successfully created';
 const MESSAGE_UPDATE = 'Category was successfully updated';
@@ -14,19 +12,27 @@ const MESSAGE_404 = 'Couldn\'t find requested category';
 
 export class CategoryController {
   public index = async (req: Request, res: Response) => {
-    const { establishmentId } = req.params;
+    try {
+      const { establishmentId } = req.params;
 
-    const categories = await Category.findAll({
-      where: {
-        establishmentId
-      }
-    });
+      const categories = await Category.findAll({
+        where: {
+          establishmentId
+        }
+      });
 
-    return res.json({
-      isSuccessful: true,
-      type: ResponseType.SUCCESS,
-      data: categories
-    })
+      return res.status(200).json({
+        isSuccessful: true,
+        type: ResponseType.SUCCESS,
+        data: categories
+      })
+    } catch (error) {
+      return res.status(500).json({
+        isSuccessful: false,
+        type: ResponseType.DANGER,
+        message: error
+      })
+    }
   };
 
   public create = async (req: Request, res: Response) => {
@@ -135,30 +141,48 @@ export class CategoryController {
   };
 
   public delete = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const options: DestroyOptions = {
-      where: { id: id },
-      limit: 1,
-    };
+    try {
+      const { id } = req.params;
 
-    await Category.findByPk(id)
-      .then((node) => {
-        if (node) {
-          Category.destroy(options)
-            .then(() => res.status(200).json({
-              isSuccessful: true, type: ResponseType.SUCCESS, data: node, message: MESSAGE_DELETE
-            }))
-            .catch((error: Error) => res.status(500).json({
-              isSuccessful: false, type: ResponseType.DANGER, message: error
-            }));
-        } else {
-          res.status(404).json({
-            isSuccessful: false, type: ResponseType.DANGER, message: MESSAGE_404
-          });
-        }
-      })
-      .catch((error: Error) => res.status(500).json({
-        isSuccessful: false, type: ResponseType.DANGER, message: error
-      }));
+      await Category.findByPk(id)
+        .then((node) => {
+          if (node) {
+            Category.destroy({
+              where: {
+                id
+              },
+              limit: 1,
+            })
+              .then(() => res.status(200).json({
+                isSuccessful: true,
+                type: ResponseType.SUCCESS,
+                data: node,
+                message: MESSAGE_DELETE
+              }))
+              .catch((error: Error) => res.status(500).json({
+                isSuccessful: false,
+                type: ResponseType.DANGER,
+                message: error
+              }));
+          } else {
+            res.status(404).json({
+              isSuccessful: false,
+              type: ResponseType.DANGER,
+              message: MESSAGE_404
+            });
+          }
+        })
+        .catch((error: Error) => res.status(500).json({
+          isSuccessful: false,
+          type: ResponseType.DANGER,
+          message: error
+        }));
+    } catch (error) {
+      res.status(500).json({
+        isSuccessful: false,
+        type: ResponseType.DANGER,
+        message: error
+      });
+    }
   };
 }
